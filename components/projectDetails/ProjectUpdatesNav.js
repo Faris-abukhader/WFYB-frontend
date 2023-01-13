9ca9e0017da053562213d1335a58ba62165bd473
Ahmed <suwediahmed@gmail.com>
2023-01-14 05:36:07 +0800

    Update ProjectUpdatesNav.js

--- components/projectDetails/ProjectUpdatesNav.js
+++ components/projectDetails/ProjectUpdatesNav.js
@@ -10,8 +10,9 @@ export default function ProjectUpdatesNav({ onClickOption, language }) {
 
     return (
         <div className='shadow-[0_5px_20px_0px_rgba(0,0,0,0.1)] mb-8'>
-            <ul className='w-full flex justify-center items-center shadow-md rounded-md font-almarai'>
-                <li onClick={() => handleClick('story')} className={`font-bold text-xl p-5 px-10 border-green-500 ${selected === 'story' && `border-b-4 text-green-500`} focus:text-green-500 hover:cursor-pointer hover:text-green-500`}>
+            <ul>
+                <li onClick={() => handleClick('story')} 
+                    className={`font-bold text-xl p-5 px-10 border-green-500 ${selected === 'story' && `border-b-4 text-green-500`} focus:text-green-500 hover:cursor-pointer hover:text-green-500`}>
                     {t('story', language)}
                 </li>
                 <li onClick={() => handleClick('commentsList')} className={`font-bold text-xl p-5 px-10 border-green-500 ${selected === 'commentsList' && `border-b-4 text-green-500`} focus:text-green-500 hover:cursor-pointer hover:text-green-500`}>
